24e4d2981b7790d2f1ce34f61de5e69e9fa1a936
Kazi Anwar <kazisameen2702@gmail.com>
2023-09-15 22:58:26 -0400

    added goHome button

---
+++ goHome.js
@@ -0,0 +1,9 @@
+// goHome.js
+
+// JavaScript code to navigate to the homepage
+const goHomeButton = document.getElementById('goHome');
+
+goHomeButton.addEventListener('click', function() {
+    // Change the URL to the homepage
+    window.location.href = 'index.html';
+});
